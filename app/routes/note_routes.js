08e71666b47d196c8283114681e8a214f0366fa2
Jose Rivas <rivass.josea@gmail.com>
2017-05-29 21:46:20 -0400

    Nuevas operaciones agregadas

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,17 @@
 var ObjectID = require('mongodb').ObjectID;
 
-module.exports = function(app, db) {
+module.exports = function(app, db) {	
+	
+   app.get('/notes', (req, res) => {
+    db.collection('notes').find({}).toArray(function (err, collection) {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {		
+        res.send(collection);
+      }
+    });
+  });
+	
    app.get('/notes/:id', (req, res) => {
 	const id = req.params.id;   
     const details = { '_id': new ObjectID(id) };
@@ -23,4 +34,30 @@ module.exports = function(app, db) {
       }
     });
   });
+  
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });  
+  
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+  
 };
